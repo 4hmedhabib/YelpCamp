83dc13d1c77bbb58927b82a5c72721c1dd582c70
ahmedhabiib <info.ahemd.developer@gmail.com>
2021-06-12 11:43:21 +0300

    modified register route

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/campground/user');
+const catchAsync = require('../utils/catchAsync')
 
 router.get('/register', (req, res, next) => {
     res.render('users/register')
 });
 
-router.post('/register', (req, res, next) => {
-    res.send(req.body)
-})
+router.post('/register', catchAsync(async(req, res, next) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to Yelp Camp!')
+        res.redirect('/campgrounds')
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('/register')
+    }
+
+}));
+
 
 module.exports = router;
