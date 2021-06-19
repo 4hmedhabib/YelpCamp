e09ee958e687f00708600fc33be76294c5c92b1c
ahmedhabiib <info.ahmed.developer@gmail.com>
2021-06-19 09:56:53 +0300

    restrucred route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -7,20 +7,16 @@ const campgrounds = require('../controllers/campgrounds')
 
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
 
-
-router.get('/', campgrounds.index);
+router.route('/')
+    .get(campgrounds.index)
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
-
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
-router.get('/:id', catchAsync(campgrounds.showCampground));
-
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
-router.put('/:id/edit', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-router.delete('/:id/delete', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
-
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const passport = require('passport')
 
 const catchAsync = require('../utils/catchAsync');
-const users = require('../controllers/users')
+const users = require('../controllers/users');
 
-router.get('/register', users.renderRegisterForm);
+router.route('/register')
+    .get(users.renderRegisterForm)
+    .post(catchAsync(users.register))
 
-router.post('/register', catchAsync(users.register));
-
-router.get('/login', users.renderLoginForm);
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+router.route('/login')
+    .get(users.renderLoginForm)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
 router.get('/logout', users.logout)
 
